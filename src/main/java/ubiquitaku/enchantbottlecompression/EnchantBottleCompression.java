512224fdd93900f80a8dc1b04eb26652a65c9be6
ubiquitaku <ubiquitaku@outlook.jp>
2021-05-22 20:31:23 +0900

    作るだけ作ったけど未確認

--- src/main/java/ubiquitaku/enchantbottlecompression/EnchantBottleCompression.java
+++ src/main/java/ubiquitaku/enchantbottlecompression/EnchantBottleCompression.java
@@ -7,12 +7,20 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 public final class EnchantBottleCompression extends JavaPlugin {
     FileConfiguration config;
     String prefix;
     boolean ebc;
+    ItemStack normalBottle = new ItemStack(Material.EXPERIENCE_BOTTLE);
+    ItemStack ebcStack = new ItemStack(Material.EXPERIENCE_BOTTLE);
+    ItemMeta ebcMeta;
+
 
     @Override
     public void onEnable() {
@@ -23,6 +31,9 @@ public final class EnchantBottleCompression extends JavaPlugin {
         ebc = config.getBoolean("EBC",false);
         saveConfig();
         config = getConfig();
+        ebcMeta.setDisplayName("§a§lEnchantmentBottle");
+        ebcMeta.setLore(Arrays.asList("圧縮されたエンチャント便","メインハンドに持っているときは使用できない","しかし不具合防止のためそれ以外では使えるが","ただのエンチャント瓶と効果は変わらない"));
+        ebcStack.setItemMeta(ebcMeta);
     }
 
     @Override
@@ -79,12 +90,45 @@ public final class EnchantBottleCompression extends JavaPlugin {
             }
             if (e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals("§a§lEnchantmentBottle")) {
                 //１個消して64個のepボトル渡す
+                if (!ebc) {
+                    e.getPlayer().sendMessage(prefix+"現在停止されているため交換できません");
+                    return;
+                }
+                e.getPlayer().getInventory().remove(ebcStack);
+                e.getPlayer().getInventory().addItem(normalBottle);
+                e.getPlayer().sendMessage(prefix+"ボトルを解凍しました");
                 return;
             }
         }
         if (e.getPlayer().getInventory().getItemInMainHand().getAmount() == 64) {
             //64個消して１個の圧縮ボトルを渡す
+            if (!ebc) {
+                e.getPlayer().sendMessage(prefix+"現在停止されているため交換できません");
+                return;
+            }
+            for (int i = 0;i < 64;i++) {
+                e.getPlayer().getInventory().removeItem(normalBottle);
+            }
+            e.getPlayer().getInventory().addItem(ebcStack);
+            e.getPlayer().sendMessage(prefix+"ボトルを圧縮しました");
         }
+    }
 
+    @EventHandler
+    public void onUse(PlayerInteractEvent e) {
+        if (e.getAction() != Action.RIGHT_CLICK_AIR && e.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        if (!e.getPlayer().getInventory().getItemInMainHand().hasItemMeta()) {
+            return;
+        }
+        if (!e.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasDisplayName()) {
+            return;
+        }
+        if (!e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals("§a§lEnchantmentBottle")) {
+            return;
+        }
+        e.getPlayer().sendMessage(prefix+"そのアイテムを使うことはできません");
+        e.setCancelled(true);
     }
 }
